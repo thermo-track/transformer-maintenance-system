708c68268d23e94d31104e9e934a2b93cf3619ae
Javin Manatunge <javinmanatunge@gmail.com>
2025-08-23 10:03:21 +0530

    CorsConifg deleted

--- tms-backend-application/src/main/java/com/powergrid/maintenance/tms_backend_application/transformer/controller/TransformerController.java
+++ tms-backend-application/src/main/java/com/powergrid/maintenance/tms_backend_application/transformer/controller/TransformerController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/transformers")
 @RequiredArgsConstructor
 public class TransformerController {
